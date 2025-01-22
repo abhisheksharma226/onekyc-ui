c1b3d9076214320ddd97171cfc2062ceba53dc75
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-01-22 23:28:49 +0530

    Show pass

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import SignUp from "./components/Signup/Signup.jsx";
 import Login from "./components/Login/Login.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 const App = () => {
   return (
     <Router>
