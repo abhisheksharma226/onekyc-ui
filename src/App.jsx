db2a2c31483a39ab701ae32b084fcde3a7ce76fa
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-01-24 15:16:22 +0530

    registration form added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import SignUp from "./components/Signup/Signup.jsx";
 import Login from "./components/Login/Login.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from "./components/Register/Register.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
       </Routes>
     </Router>
   );
